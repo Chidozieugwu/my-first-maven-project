185fd659c12343b6c8f16a66b19f73fd52f3e4e8
prof prince <prince@liontechacademy.com>
2024-02-17 22:59:00 +0100

    my first maven project

---
+++ sportsapp/src/test/java/com/chidozieliontech/sportsapp/SportsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chidozieliontech.sportsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
